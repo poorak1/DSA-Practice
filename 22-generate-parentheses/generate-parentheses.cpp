4b021bdf3477d75cae32c493c3a47613e7f33247
Poorak Pasrija <132227473+poorak1@users.noreply.github.com>
2026-03-21 22:43:26 +0530

    Time: 3 ms (69.79%) | Memory: 12.9 MB (88.92%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,53 +1,28 @@
 class Solution {
 public:
-    bool valid_parenthesis(string &curr_str){
-        stack<int> st;
-        for(char &c: curr_str){
-            if(c == '(') st.push(')');
-            else if(c == '{') st.push('}');
-            else if(c == '[') st.push(']');
-            else if(st.empty() || c!=st.top()) return false;
-            else st.pop();
-        }
-        return st.empty();
-    }
-
-    void helper(vector<string> &result, string &curr_str, int curr_open, int curr_closed){
-        if((!curr_open) && (!curr_closed)){
-            if(valid_parenthesis(curr_str)) result.push_back(curr_str);
-            return;    
-        }
-
-        if(curr_open == 0){
-            curr_str = curr_str + string (curr_closed, ')');
-            helper(result, curr_str, 0, 0);
-            curr_str = curr_str.substr(0, curr_str.size() - curr_closed); 
+    void helper(vector<string> &result, string &curr, int open, int close){
+        if(open == 0 && close == 0){
+            result.push_back(curr);
             return;
         }
 
-        if(curr_closed == 0){
-            curr_str = curr_str + string (curr_open, '(');
-            helper(result, curr_str, 0, 0);
-            curr_str = curr_str.substr(0, curr_str.size() - curr_open); 
-            return;
+        if(open > 0){
+            curr.push_back('(');
+            helper(result, curr, open - 1, close);
+            curr.pop_back();
         }
 
-        curr_str = curr_str + "(";
-        helper(result, curr_str, curr_open-1, curr_closed);
-        curr_str.pop_back();
-        
-        curr_str = curr_str + ")";
-        helper(result, curr_str, curr_open, curr_closed-1);
-        curr_str.pop_back();
+        if(close > open){
+            curr.push_back(')');
+            helper(result, curr, open, close - 1);
+            curr.pop_back();
+        }
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string> result;
-        string curr_str;
-        int open_braces = n;
-        int closed_braces = n;
-
-        helper(result, curr_str, open_braces, closed_braces);
+        string curr;
+        helper(result, curr, n, n);
         return result;
     }
 };
